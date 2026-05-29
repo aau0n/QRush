f3dfa4b2f20393da76cd765a252b9185094870a6
JAY <aau0n@ewha.ac.kr>
2026-05-29 15:34:23 +0900

    Backend 기본 작업
    폴더 정리
    Express server
    MongoDB 연결
    Mongoose 연결
    .env 연결

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
-const app = require("./app");
+require("dotenv").config();
 
-const PORT = 3000;
+const app = require("./app");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
